ce584960b33adfa4fd0ae4fb67c472f5bffc85a2
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-03-10 17:42:16 +0800

    Create classes for Particles

---
+++ src/main/java/com/equinoxtic/vertiliumores/init/ParticlesInit.java
@@ -0,0 +1,8 @@
+package com.equinoxtic.vertiliumores.init;
+
+import com.equinoxtic.vertiliumores.particles.ParticleCustom;
+
+public class ParticlesInit
+{
+    public static final ParticleCustom.TextureDefinition SPARKLE = new ParticleCustom.TextureDefinition("sparkle", true, 3);
+}
