c3a16b85ebbc35276532edea8b91b88e7a62d0f3
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-04-06 00:34:15 +0800

    Create Vertilium Pickaxe

--- src/main/java/com/equinoxtic/vertiliumores/init/ItemInit.java
+++ src/main/java/com/equinoxtic/vertiliumores/init/ItemInit.java
@@ -1,7 +1,10 @@
 package com.equinoxtic.vertiliumores.init;
 
 import com.equinoxtic.vertiliumores.objects.items.IngotVertilium;
+import com.equinoxtic.vertiliumores.objects.tools.ToolPickaxe;
 import net.minecraft.item.Item;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraftforge.common.util.EnumHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,5 +13,18 @@ public class ItemInit
 {
     public static final List<Item> ITEMS = new ArrayList<Item>();
 
+    // Materials
+    public static final ToolMaterial TOOL_VERTILIUM = EnumHelper.addToolMaterial(
+            "tool_vertilium",
+            4,
+            2342,
+            8.0F,
+            12.0F,
+            12);
+
+    // Items
     public static final Item VERTILIUM = new IngotVertilium();
+
+    // Tools
+    public static final Item PICKAXE_VERTILIUM = new ToolPickaxe("pickaxe_vertilium", TOOL_VERTILIUM);
 }
---
+++ src/main/java/com/equinoxtic/vertiliumores/objects/tools/ToolPickaxe.java
@@ -0,0 +1,24 @@
+package com.equinoxtic.vertiliumores.objects.tools;
+
+import com.equinoxtic.vertiliumores.Main;
+import com.equinoxtic.vertiliumores.init.ItemInit;
+import com.equinoxtic.vertiliumores.util.interfaces.IHasModel;
+import net.minecraft.item.ItemPickaxe;
+
+public class ToolPickaxe extends ItemPickaxe implements IHasModel
+{
+    public ToolPickaxe(String name, ToolMaterial material) {
+        super(material);
+        setUnlocalizedName(name);
+        setRegistryName(name);
+        setCreativeTab(Main.VERTILIUM_ORES);
+        setHarvestLevel("pickaxe", 4);
+
+        ItemInit.ITEMS.add(this);
+    }
+
+    @Override
+    public void registerModels() {
+        Main.proxy.registerItemRenderer(this, 0, "inventory");
+    }
+}
