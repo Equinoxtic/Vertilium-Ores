ec058e3672687acd1b05cd04561ae1cd5ed620ff
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-04-23 01:18:27 +0800

    Update name for Vertilium Pickaxe

--- src/main/java/com/equinoxtic/vertiliumores/init/ItemInit.java
+++ src/main/java/com/equinoxtic/vertiliumores/init/ItemInit.java
@@ -1,7 +1,9 @@
 package com.equinoxtic.vertiliumores.init;
 
 import com.equinoxtic.vertiliumores.objects.items.IngotVertilium;
+import com.equinoxtic.vertiliumores.objects.tools.ToolAxe;
 import com.equinoxtic.vertiliumores.objects.tools.ToolPickaxe;
+import com.equinoxtic.vertiliumores.objects.tools.ToolSword;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.ToolMaterial;
 import net.minecraftforge.common.util.EnumHelper;
@@ -17,15 +19,45 @@ public class ItemInit
     public static final ToolMaterial PICKAXE_MATERIAL = EnumHelper.addToolMaterial(
             "tool_pickaxe_vertilium",
             4, // Harvest Level
+            2576, // Durability
+            18.0F, // Efficiency
+            9.0F, // Base Damage
+            22 // Enchantability
+    );
+
+    public static final ToolMaterial SWORD_MATERIAL = EnumHelper.addToolMaterial(
+            "tool_sword_vertilium",
+            4, // Harvest Level
+            3044, // Durability
+            12.0F, // Efficiency
+            18.0F, // Base Damage
+            24 // Enchantability
+    );
+
+    public static final ToolMaterial AXE_MATERIAL = EnumHelper.addToolMaterial(
+            "tool_axe_vertilium",
+            4, // Harvest Level
+            2810, // Durability
+            16.0F, // Efficiency
+            17.0F, // Base Damage
+            23 // Enchantability
+    );
+
+    public static final ToolMaterial HOE_MATERIAL = EnumHelper.addToolMaterial(
+            "tool_hoe_vertilium",
+            4, // Harvest Level
             2342, // Durability
-            15.0F, // Efficiency
+            18.0F, // Efficiency
             12.0F, // Base Damage
-            12 // Enchantability
+            21 // Enchantability
     );
 
     // Items
     public static final Item VERTILIUM = new IngotVertilium();
 
     // Tools
-    public static final Item PICKAXE_VERTILIUM = new ToolPickaxe("pickaxe_vertilium", PICKAXE_MATERIAL);
+    public static final Item VERTILIUM_PICKAXE = new ToolPickaxe("vertilium_pickaxe", PICKAXE_MATERIAL);
+    public static final Item VERTILIUM_SWORD = new ToolSword("vertilium_sword", SWORD_MATERIAL);
+    public static final Item VERTILIUM_AXE = new ToolAxe("vertilium_axe", AXE_MATERIAL);
+    public static final Item VERTILIUM_HOE = new ToolAxe("vertilium_hoe", HOE_MATERIAL);
 }
